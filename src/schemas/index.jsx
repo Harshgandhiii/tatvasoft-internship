aa87fc349a4cb7dd9725ea491b5563e98642a9dc
Harshgandhiii <109617469+Harshgandhiii@users.noreply.github.com>
2023-06-07 11:10:58 +0530

    Add files via upload

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -1,17 +1,19 @@
 import * as Yup from 'yup';
 
 export const signUpSchema = Yup.object({
-    fname:Yup.string().min(2).max(25).required("Please enter your first name"),
-    lname:Yup.string().min(2).max(25).required("Please enter your last name"),
+    firstName:Yup.string().min(2).max(25).required("Please enter your first name"),
+    lastName:Yup.string().min(2).max(25).required("Please enter your last name"),
     email:Yup.string().email().required("Please enter your Email"),
-    pass:Yup.string().min(6).required("Please enter your password"),
-    pass1:Yup.string().required().oneOf([Yup.ref('pass'),null],"Password must match"),
+    roleId: Yup.string().required("Role is required"),
+    password:Yup.string().min(6).required("Please enter your password"),
+    confirmPassword:Yup.string().required().oneOf([Yup.ref('password'),null],"Password must match"),
     
     
     
 })
 
 export const loginSchema =Yup.object({
-    Email:Yup.string().min(2).max(25).required("Please enter your Email"),
-    Password:Yup.string().min(2).max(25).required("Please enter your Password"),
+    email:Yup.string().min(2).max(25).required("Please enter your Email"),
+    password:Yup.string().min(2).max(25).required("Please enter your Password"),
 })
+
